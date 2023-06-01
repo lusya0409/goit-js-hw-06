77a21c212898fa6e5edda9ba191025cc7161b359
lusya0409 <lusya0409@gmail.com>
2023-06-01 19:28:28 +0300

    make-task-03.js and common.css

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,16 @@
+// Напиши скрипт для створення галереї зображень на підставі масиву даних.
+// HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+//   Використовуй масив об'єктів images для створення елементів <img>,
+//  вкладених в < li >.Для створення розмітки використовуй шаблонні рядки
+//   і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через
+//  CSS класи.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +25,23 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listEl = document.querySelector('.gallery');
+
+const createImageMarkup = images => {
+  return images
+    .map(
+      ({ url, alt }) =>
+        `<li><img class="gallary-item" src="${url}" alt="${alt}" /></li>`
+    )
+    .join('');
+};
+// const items = createImageMarkup(images);
+// listEl.insertAdjacentHTML('beforeend', items);
+listEl.insertAdjacentHTML('beforeend', createImageMarkup(images));
+
+// const itemImageMarkup = images
+//   .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" /></li>`)
+//   .join('');
+// listEl.insertAdjacentHTML('beforeend', itemImageMarkup);
+console.log(listEl);
